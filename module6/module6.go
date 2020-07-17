63cb986fa665d6505b171377a11e1cda8c5d2643
Ilho Song <ilho.song@siemens.com>
2020-07-17 15:20:43 -0400

    "feat: module6"

--- module6/module6.go
+++ module6/module6.go
@@ -1,4 +1,9 @@
 package module6
 
+//FunctionForModule6GoDoc is a function for document sample
+// multi line?
+//
+// asdlijilajsildjailsdjilqjwdilqjwildjqiwldj
+// qwldijqilwdjilqw
 func FunctionForModule6GoDoc() {
 }
